d7d53ea5fa79a5ae2ecad6d8820a9c787c202f7f
sadeghjalali <s.jalali.tsu@gmail.com>
2019-04-08 12:55:20 +0330

    Updates

---
+++ precache-manifest.ab29c3cbf214f1bbc02b779b02eaa7d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fa7798e0f83aafb75f7",
+    "url": "/stopwatch/static/js/runtime~main.84f376ef.js"
+  },
+  {
+    "revision": "0527de5c56dc587cfece",
+    "url": "/stopwatch/static/js/main.c3aa3f9c.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/stopwatch/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "0527de5c56dc587cfece",
+    "url": "/stopwatch/static/css/main.6cb610a7.chunk.css"
+  },
+  {
+    "revision": "02d321fd71d02481c688b08ffa03bb4c",
+    "url": "/stopwatch/index.html"
+  }
+];
