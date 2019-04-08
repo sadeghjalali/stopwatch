27cea97e674c320664bee343bf94f3e1249c0b52
sadeghjalali <s.jalali.tsu@gmail.com>
2019-04-08 12:43:22 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Stopwatch</h1>
+        <Stopwatch />
+      </div>
+    );
+  }
+}
+
+class Stopwatch extends Component {
+  state = {
+    status: false,
+    runningTime: 0
+  };
+  handleClick = () => {
+    this.setState(state => {
+      if (state.status) {
+        clearInterval(this.timer);
+      } else {
+        const startTime = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({ runningTime: Date.now() - startTime });
+        });
+      }
+      return { status: !state.status };
+    });
+  };
+  handleReset = () => {
+    clearInterval(this.timer); // new
+    this.setState({ runningTime: 0, status: false });
+  };
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  render() {
+    const { status, runningTime } = this.state;
+    return (
+      <div>
+        <h1 className='ms'>{runningTime}ms</h1>
+        <button onClick={this.handleClick} className='stopandreset'>{status ? 'Stop' : 'Start'}</button>
+        <button onClick={this.handleReset} className='stopandreset'>Reset</button>
       </div>
     );
   }
